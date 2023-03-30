7ba191d80155a5e8913e08a072ebd764227e7f97
vaibhav Name <you@example.com>
2023-03-30 19:23:39 +0530

    update

--- vaibhavang/src/app/directives/directives.component.ts
+++ vaibhavang/src/app/directives/directives.component.ts
@@ -6,8 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./directives.component.scss']
 })
 export class DirectivesComponent {
-para:any=true; //para=undefined/false=false.
+para:any=true; //para=undefined/false=false.   //ngIf directives
 show:any=true;
+item=[10,20,30,40,50];//*ngfor directives
+studentData:boolean=true;
+  student = [{   //*ngfor directives
+
+    name: 'rahul',
+    age: 20,
+    hobby: 'cricket'
+  },
+  {
+    name: 'rohit',
+    age: 21,
+    hobby: 'swimiing'
+  },
+  {
+    name: 'pawan',
+    age: 20,
+    hobby: 'dancing'
+  },
+  {
+    name: 'kalyan',
+    age: 20,
+    hobby: 'printing'
+  }
+  ]
 
 hide()
 {
@@ -23,4 +47,41 @@ togglehideshow()
 {
   this.show=!this.show;
 }
+
+imgshowhide:boolean=true;
+showimg()
+{
+  this.imgshowhide=true;
+}
+
+hideimg()
+{
+  this.imgshowhide=false;
+}
+
+toggleimg()
+{
+  this.imgshowhide=!this.imgshowhide;
+}
+
+//ngswitch directives
+color="green";
+
+switchfun(color:any)
+{
+     this.color=color;
+}
+
+switchfun1(color:any)
+{
+    this.color=color;
+}
+
+//attribute directives
+styleProperty="main2";
+// stylePropertyNew=true;
+// stylePropertyNew=undefined;
+stylePropertyNew=555;
+
+styleMultiple='kabade'; //multiplestyple apply
 }
